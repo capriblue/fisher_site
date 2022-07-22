066fdf99b9f80d68e629201251bbc1bda5727c8d
capriblue <capri4wrk@gmail.com>
2022-07-23 05:39:21 +0900

    material uiの導入のための準備

--- viewer/pages/_app.js
+++ viewer/pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import PropTypes from "prop-types";
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
+import createEpotionCache from '../src/createEmotionCache';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEpotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Component {...pageProps}/>
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
+// import '../styles/globals.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired
+}
 export default MyApp
